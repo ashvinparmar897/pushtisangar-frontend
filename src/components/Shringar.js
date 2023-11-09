2af4b8a511e7626767514a750220d9ea4cdd3634
shalin sheth <shalinsheth4915@gmail.com>
2023-11-09 20:31:57 +0530

    shringar slider

--- src/components/Shringar.js
+++ src/components/Shringar.js
@@ -86,28 +86,40 @@ const Shringar = () => {
 
   
   var settings = {
-    // other settings...
-    slidesToShow: 4, // Show 4 slides on larger screens
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1025,
+        breakpoint: 1600,
         settings: {
-          slidesToShow: 3, // Show 3 slides on medium-sized screens
+          slidesToShow: 4,
           slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2, // Show 2 slides on small screens
-          slidesToScroll: 1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1, // Show 1 slide on extra small screens
+          slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
@@ -183,7 +195,7 @@ const Shringar = () => {
             </div>
           </div>
 
-          <div className="col-lg-9 col-md-8 col-sm-6 mb-4">
+          <div className="col-lg-9 col-md-12 mb-4">
             <Slider {...settings}>
               {products.slice(0, 4).map((product) => (
                 <div key={product.id}>
